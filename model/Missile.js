8d9ba363e27e4c7d3178294130d1d87deeac869c
Lizzy H <lizzyh@Lizzys-MBP.home>
2022-03-24 18:40:30 -0400

    Updated Missile Class with constructor

---
+++ model/Missile.js
@@ -0,0 +1,8 @@
+import Sprite from "./Sprite.js"
+
+class Missile extends Sprite{
+    constructor(x,y) {
+        super(x, y, 10, 20, 0, -5);
+        this.image.src = "./assets/missile.png";
+    }
+}
